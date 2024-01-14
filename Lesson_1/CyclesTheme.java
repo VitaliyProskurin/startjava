672a4856b151f18c72fbcdef9cd61250abc23379
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-01-14 11:52:48 +0300

    CyclesTheme Corrections 1

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -1,38 +1,39 @@
 public class CyclesTheme {
     public static void main(String[] args) {
         System.out.println("TASK 1 Подсчет суммы четных и нечетных чисел");
-        int a = -10;
+        int beginNumber = -10;
+        int endNumber = 21;
+        int count = beginNumber;
         int sumEvenNumbers = 0;
         int sumOddNumbers = 0;
         do {
-            if (a % 2 == 0) {
-                sumEvenNumbers += a;
-                a++;
+            if (count % 2 == 0) {
+                sumEvenNumbers += count;
             } else {
-                sumOddNumbers +=  a;
-                a++;
+                sumOddNumbers += count;
             }
-        } while (a <= 21);
-        System.out.println("В отрезке [-10, 21] сумма четных чисел = " + sumEvenNumbers + ", " 
-                + " а нечетных = " + sumOddNumbers);
+            count++;
+        } while (count <= endNumber);
+        System.out.println("В отрезке [" + beginNumber + ", "+ endNumber 
+            + "] сумма четных чисел = " + sumEvenNumbers + ", " + " а нечетных = " + sumOddNumbers);
         
         System.out.println("\nTASK 2 Вывод чисел в порядке убывания");
-        int b = 10;
-        int c = 5;
-        int d = -1;
-        int min = b;
-        int max = b;
-        if (c < min) {
-            min = c;
+        int digit1 = 10;
+        int digit2 = 5;
+        int digit3 = -1;
+        int min = digit1;
+        int max = digit1;
+        if (digit2 < min) {
+            min = digit2;
         }
-        if (d < min) {
-            min = d;
+        if (digit3 < min) {
+            min = digit3;
         }
-        if (c > max) {
-            max = c;
+        if (digit2 > max) {
+            max = digit2;
         }
-        if (d > max) {
-            max = d;
+        if (digit3 > max) {
+            max = digit3;
         }
         for (int i = max - 1; i > min; i--) {
             System.out.print(i + " ");
@@ -40,24 +41,23 @@ public class CyclesTheme {
 
         System.out.println("\n\nTASK 3 Вывод реверсивного числа и суммы его цифр");
         int number = 1234;
-        int sumNumbers = 0;
-        int reverseNumber = 0;
+        int sumNumbers = 0;        
+        System.out.print("Исходное число в обратном порядке: ");
         while (number != 0) {
-            reverseNumber = number % 10 + reverseNumber * 10;
-            sumNumbers += number % 10;
+            int reverseNumber = number % 10;
+            System.out.print(reverseNumber);
+            sumNumbers += reverseNumber;
             number = number / 10;
         }
-        System.out.println("Реверсивное число: " + reverseNumber 
-                + " Сумма чисел числа: " + sumNumbers);
+        System.out.println("\nСумма цифр числа: " + sumNumbers);
 
         System.out.println("\nTASK 4 Вывод чисел в несколько строк");
         min = 1;
-        max = 30;
-        int oddNumber = 0;
-        int count = 0;
+        max = 24;
+        count = 0;
         for (int i = min; i < max; i++ ) {
-            if (count % 5 == 0) {
-                System.out.println("");
+            if (i % 2  !=0 && count % 5 == 0) {
+                System.out.println();
             } 
             if (i % 2 != 0) {
                 System.out.printf("%4d", i);
@@ -69,25 +69,24 @@ public class CyclesTheme {
                     System.out.printf("%4d", 0);
                     count++;
                 }
-            }
+            }            
         }
 
         System.out.println("\n\nTASK 5 Проверка количества двоек числа на четность/нечетность");
         number = 3242592;
-        int tempNumber = number;
-        int tempDigit;
-        count = 0;
-        while (tempNumber != 0) {
-            tempDigit = tempNumber % 10;
-            if (tempDigit == 2) {
-                count++;
+        int copyNumber = number;
+        int countTwos = 0;
+        while (copyNumber != 0) {
+            int digit = copyNumber % 10;
+            if (digit  == 2) {
+                countTwos++;
             }
-            tempNumber = tempNumber / 10;
+            copyNumber = copyNumber / 10;
         }
-        if (count % 2 == 0) {
-            System.out.println("В " + number + " четное количество двоек - " + count);
+        if (countTwos % 2 == 0) {
+            System.out.println("В " + number + " четное количество двоек - " + countTwos);
         } else {
-            System.out.println("В " + number + " нечетное количество двоек - " + count);
+            System.out.println("В " + number + " нечетное количество двоек - " + countTwos);
         }
 
         System.out.println("\nTASK 6 Отображение геометрических фигур");
@@ -99,15 +98,15 @@ public class CyclesTheme {
         }
 
         System.out.println("\n");
-        a = 0;
+        int a = 0;
         count = 5;
-        int temp = count;
+        copyNumber = count;
         while (a < 15) {
-            if (a == temp) {
-                System.out.println("");
+            if (a == copyNumber) {
+                System.out.println();
                 count--;
-                temp = a;
-                temp += count;
+                copyNumber = a;
+                copyNumber += count;
             } 
             System.out.print("#");
             a++;
@@ -116,28 +115,28 @@ public class CyclesTheme {
         System.out.println("\n");
         a = 0;
         count = 1;
-        temp = count;
+        copyNumber = count;
         do {
-            if (a == temp) {
-                System.out.println("");
+            if (a == copyNumber) {
+                System.out.println();
                 count++;
-                temp = a;
-                temp += count;
+                copyNumber = a;
+                copyNumber += count;
             }
             System.out.print("$");
             a++;
         } while (a <= 9);
-        System.out.println("");
-        b = a - count;
+        System.out.println();
+        int b = a - count;
         a = 0;
         count = count - 1;
-        temp = count;
+        copyNumber = count;
         do {
-            if (a == temp) {
-                System.out.println("");
+            if (a == copyNumber) {
+                System.out.println();
                 count--;
-                temp = a;
-                temp += count;
+                copyNumber = a;
+                copyNumber += count;
             } 
             System.out.print("$");
             a++;
@@ -168,11 +167,11 @@ public class CyclesTheme {
 
         System.out.println("\nTASK 8 Проверка, является ли число палиндромом");
         number = 1234321;
-        reverseNumber = 0;
-        temp = number;
-        while (temp !=0) {
-            reverseNumber = temp % 10 + reverseNumber * 10;
-            temp = temp / 10;
+        int reverseNumber = 0;
+        copyNumber = number;
+        while (copyNumber != 0) {
+            reverseNumber = copyNumber % 10 + reverseNumber * 10;
+            copyNumber = copyNumber / 10;
         }
         if (reverseNumber == number) {
             System.out.printf("Число %d является палиндромом", number);
@@ -183,11 +182,11 @@ public class CyclesTheme {
         System.out.println("\n\nTASK 9 Проверка, является ли число счастливым");
         number = 123456;
         reverseNumber = 0;
-        temp = number;        
+        copyNumber = number;
         count = 0;
-        while (temp !=0) {
-            reverseNumber = temp % 10 + reverseNumber * 10;
-            temp = temp / 10;
+        while (copyNumber !=0) {
+            reverseNumber = copyNumber % 10 + reverseNumber * 10;
+            copyNumber = copyNumber / 10;
             count++;
         }
         int halfCount = count / 2;
@@ -195,15 +194,15 @@ public class CyclesTheme {
         int sumNumbers2 = 0;
         while (count != 0) {
             if (count > halfCount) {
-                temp = reverseNumber % 10;
+                copyNumber = reverseNumber % 10;
                 reverseNumber = reverseNumber / 10;
                 count--;
-                sumNumbers1 += temp;
+                sumNumbers1 += copyNumber;
             } else {
-                temp = reverseNumber % 10;
+                copyNumber = reverseNumber % 10;
                 reverseNumber = reverseNumber / 10;
                 count--;
-                sumNumbers2 += temp;
+                sumNumbers2 += copyNumber;
             }
         }
         if (sumNumbers1 == sumNumbers2) {

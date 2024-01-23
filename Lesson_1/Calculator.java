55a695797655a1753d94429e48a50df068ca01c2
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-01-23 16:55:15 +0300

    Source code

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        char sign = '^';
+        int result = 0;
+        if (sign == '+') {
+            result = a + b;
+        } else if (sign == '-') {
+            result = a - b;
+        } else if (sign == '*') {
+            result = a * b;
+        } else if (sign == '/') {
+            if (b == 0) {
+                System.out.println("Делить на ноль нельзя");
+            } else {
+                result = a / b; 
+            }
+        } else if (sign == '^') {
+            result = a;
+            for (int i = 1; i < b; i++) {
+                result *= a;
+            }
+        } else if (sign == '%') {
+            result = a % b;
+        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+}

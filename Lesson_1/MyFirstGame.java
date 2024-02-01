b14ab4a0eb243f629a75194f1423cdc1d85dbc40
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-01 23:39:40 +0300

    MyFirstGame Corrections 1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,14 +1,14 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int numberPc = 56;
-        int numberUser = 60;
-        while (numberUser != numberPc) {
-            if (numberUser > numberPc) {
-                System.out.println("Число " + numberUser + " больше того, что загадал компьютер");
-                numberUser--;
-            } else if (numberUser < numberPc) {
-                System.out.println("Число " + numberUser + " меньше того, что загадал компьютер");
-                numberUser++;
+        int secretNum = 56;
+        int playerNumber = 60;
+        while (playerNumber != secretNum) {
+            if (playerNumber > secretNum) {
+                System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
+                playerNumber--;
+            } else if (playerNumber < secretNum) {
+                System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
+                playerNumber++;
             }
         }
         System.out.println("Вы победили!");

6d7d19a8279b964e3b0364ce48485d226b133d30
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-03 15:21:21 +0300

    Added Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person  {
+
+    String gender = "мужской";
+    String name = "Виталий";
+    float height = 1.85f;
+    float weight = 92f;
+    int age = 35;
+
+    void move() {
+        System.out.println("идет");
+    }
+
+    void seat() {
+        System.out.println("сидит");
+    }
+
+    void run() {
+        System.out.println("бежит");
+    }
+
+    void speak() {
+        System.out.println("говорит");
+    }
+
+    void learn() {
+        System.out.println("учит Java");
+    }
+}

1edcd4727319a9a2ec88d2cf1ba9e7bedc4f1034
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-04 12:02:25 +0300

    Add WolfTest.java

---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,23 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolfOne = new Wolf();
+        wolfOne.gender = "Мужская особь";
+        wolfOne.nickname = "Бракус";
+        wolfOne.weight = 40.5f;
+        wolfOne.age = 10;
+        wolfOne.color = "Серый";
+
+        System.out.println("Пол = " + wolfOne.gender);
+        System.out.println("Кличка = " + wolfOne.nickname);
+        System.out.println("Вес = " + wolfOne.weight);
+        System.out.println("Возраст = " + wolfOne.age);
+        System.out.println("Окрас = " + wolfOne.color);
+        
+        wolfOne.move();
+        wolfOne.seat();
+        wolfOne.run();
+        wolfOne.howl();
+        wolfOne.hunt();
+    }
+}

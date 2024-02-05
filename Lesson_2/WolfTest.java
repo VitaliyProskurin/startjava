f99d72bc728618c94296d551e8ddbd3ecb01c0c3
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-05 10:52:38 +0300

    Second task WolfTest.java

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,17 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.gender = "Мужская особь";
-        wolfOne.nickname = "Бракус";
-        wolfOne.weight = 40.5f;
-        wolfOne.age = 10;
-        wolfOne.color = "Серый";
+        wolfOne.setGender("Мужская особь");
+        wolfOne.setNickname("Бракус");
+        wolfOne.setWeight(40.5f);
+        wolfOne.setAge(10);
+        wolfOne.setColor("Серый");
 
-        System.out.println("Пол = " + wolfOne.gender);
-        System.out.println("Кличка = " + wolfOne.nickname);
-        System.out.println("Вес = " + wolfOne.weight);
-        System.out.println("Возраст = " + wolfOne.age);
-        System.out.println("Окрас = " + wolfOne.color);
+        System.out.println("Пол = " + wolfOne.getGender());
+        System.out.println("Кличка = " + wolfOne.getNickname());
+        System.out.println("Вес = " + wolfOne.getWeight());
+        System.out.println("Возраст = " + wolfOne.getAge());
+        System.out.println("Окрас = " + wolfOne.getColor());
         
         wolfOne.move();
         wolfOne.sit();

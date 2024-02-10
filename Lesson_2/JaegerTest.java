29ad0d2a9f19ed81eb077d7a945f2d760da31fa2
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-11 00:41:14 +0300

    Add JaegerTest.java

---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,27 @@
+
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger();
+        jaegerOne.setModelName("Bracer Phoenix");
+        jaegerOne.setMark("Mark-5");
+        jaegerOne.setOrigin("China");
+        jaegerOne.setHeight(70.91f);
+        jaegerOne.setWeight(2.2f);
+        jaegerOne.setSpeed(25);
+        jaegerOne.setStrength(75);
+        jaegerOne.setArmor(55);
+        jaegerOne.displayInfo();
+        jaegerOne.drift();
+        jaegerOne.move();
+        jaegerOne.scanKaiju();
+        jaegerOne.useVortexCannon();
+
+
+        Jaeger jaegerTwo = new Jaeger("Guardian Bravo", "Mark-6", "Japan", 73.21f, 
+            2.18f, 35, 70, 60);
+        jaegerTwo.displayInfo();
+        jaegerTwo.drift();
+        jaegerTwo.useVortexCannon();
+        jaegerTwo.useCoreSalvoCharges();        
+    }
+}

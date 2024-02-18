0adc2837cde4678305a22830747094113e127c24
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-18 18:15:18 +0300

    Add Player.java

---
+++ Lesson_2/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+
+    private String name;
+    private int playerNumber;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+}

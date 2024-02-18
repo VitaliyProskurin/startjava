e71c2731e33ecf6568d4aa213610e31a140fa400
Vitaliy Proskurin <vitaliyproskurin88@gmail.com>
2024-02-18 18:19:38 +0300

    Add GuessNumberTest.java

---
+++ Lesson_2/GuessNumberTest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessNumberTest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String choice;
+        System.out.println("Введите имя первого игрока: ");
+        Player player1 = new Player(scan.nextLine());
+        System.out.println("Введите имя второго игрока: ");
+        Player player2 = new Player(scan.nextLine());
+        GuessNumber GuessNumbers = new GuessNumber(player1, player2);
+        
+        do {
+            GuessNumbers.play();
+            
+            do {
+                System.out.println("\nХотите заново начать игру? [yes/no]: ");
+                choice = scan.nextLine();
+            } while (!choice.equals("no") && !choice.equals("yes"));
+        } while (choice.equals("yes"));
+    }
+}
